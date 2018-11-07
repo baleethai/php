cec7e612d7ee9380a7cbb677ecdf75c3ea8cfec3
Thinnakorn Pattha <baleethai@gmail.com>
2018-11-07 12:08:17 +0700

    updated shipping

--- shipping.php
+++ shipping.php
@@ -6,25 +6,30 @@ function compare_price($a, $b)
 {
     return strnatcmp($a['price'], $b['price']);
 }
+function compare_box($a, $b)
+{
+    return strnatcmp($a['sort'], $b['sort']);
+}
 
 // result : cost, description boxes
 $boxes = [
-	['title' => '20kg', 'price' => '300', 'weight' => '20'],
+	['title' => '3kg', 'price' => '100', 'weight' => '3'],
     ['title' => '10kg', 'price' => '200', 'weight' => '10'],
-    ['title' => '3kg', 'price' => '100', 'weight' => '3'],
+    ['title' => '20kg', 'price' => '300', 'weight' => '20'],
 ];
 
 $weight = isset($_GET['weight']) ? $_GET['weight'] : 0; // kg.
 
 $useBoxs = [];
-foreach ($boxes as $box) {
+foreach ($boxes as $key => $box) {
     $countBox = ceil($weight / $box['weight']);
     $useBoxes[] = [
         'title' => $box['title'],
         'box'   => $countBox,
-        'price' => $box['price'] * $countBox
+        'price' => $box['price'] * $countBox,
+        'sort' => $key
     ];
 }
-s($useBoxes);
 usort($useBoxes, 'compare_price');
-s(array_shift($useBoxes));
+usort($useBoxes, 'compare_box');
+s($useBoxes);
