83ae9a711e29e8083cdb7325f8a8c790f3723ab4
Thinnakorn Pattha <baleethai@gmail.com>
2018-11-11 19:13:26 +0700

    updated shipping

--- shipping.php
+++ shipping.php
@@ -2,29 +2,71 @@
 
 require 'vendor/autoload.php';
 
-function compare_price($a, $b)
-{
-    return strnatcmp($a['price'], $b['price']);
-}
-
 $boxes = [
-	['title' => '3kg', 'price' => '100', 'weight' => '3'],
-    ['title' => '10kg', 'price' => '200', 'weight' => '10'],
-    ['title' => '20kg', 'price' => '300', 'weight' => '20'],
+	['title' => '3kg', 'price' => '160', 'weight' => '3'],
+    ['title' => '10kg', 'price' => '180', 'weight' => '10'],
+    ['title' => '20kg', 'price' => '230', 'weight' => '20'],
 ];
 
 $weight = isset($_GET['weight']) ? $_GET['weight'] : 0; // kg.
+$priceCost = cost($boxes, $weight);
+s($priceCost);
+function cost($boxes = array(), $weight = 0)
+{
+    $listBox = array_filter($boxes, function($box) use($weight) {
+        return $box['weight'] >= $weight;
+    });
 
-$useBoxs = [];
-foreach ($boxes as $key => $box) {
-    $countBox = ceil($weight / $box['weight']);
-    $useBoxes[] = [
-        'title' => $box['title'],
-        'box'   => $countBox,
-        'price' => $box['price'] * $countBox,
-        'sort' => $key
-    ];
+    if ($listBox) {
+        $useBoxes = array();
+        $number = 0;
+        foreach ($listBox as $row) {
+            if ($number == 0) {
+                $useBoxes = [
+                    'title'  => $row['title'],
+                    'box'    => 1,
+                    'price'  => (float) $row['price'],
+                    'weight' => (float) $row['weight']
+                ];
+            }
+            $number++;
+        }
+        return $useBoxes;
+    }
+
+    $box = end($boxes);
+    if (is_float($weight/20)) {
+        // 20
+        $useBoxes[] = [
+            'title'  => $box['title'],
+            'box'    => floor($weight/20),
+            'price'  => $box['price'] * floor($weight/20),
+            'weight' => $box['weight']
+        ];
+        $leftBox = $weight%20;
+        $listLeftBox = array_filter($boxes, function($box) use($leftBox) {
+            return $box['weight'] >= $leftBox;
+        });
+        $no = 0;
+        foreach ($listLeftBox as $row) {
+            if ($no == 0) {
+                $useBoxes[] = [
+                    'title'  => $row['title'],
+                    'box'    => 1,
+                    'price'  => (float) $row['price'],
+                    'weight' => (float) $row['weight']
+                ];
+            }
+            $no++;
+        }
+    } else {
+        $useBoxes[] = [
+            'title'  => $box['title'],
+            'box'    => $weight/20,
+            'price'  => $box['price'] * $weight/20,
+            'weight' => $box['weight']
+        ];
+    }
+
+    return $useBoxes;
 }
-usort($useBoxes, 'compare_price');
-
-print_r($useBoxes);
