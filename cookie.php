57bf5dae450e011ef36a61f067e79f4cefc948ba
Thinnakorn Pattha <baleethai@gmail.com>
2018-12-16 23:03:18 +0700

    updated cookie

--- cookie.php
+++ cookie.php
@@ -1,7 +1,11 @@
 <?php
 
 require 'vendor/autoload.php';
+$data = [
+	'name' => 'Tum',
+	'age'  => 30
+];
 
-setcookie('name', 'Tum', time() + (86400 * 30), "/"); // 86400 = 1 day
+setcookie('name', json_encode($data), time() + (86400 * 30), "/"); // 86400 = 1 day
 
 s($_COOKIE['name']);
