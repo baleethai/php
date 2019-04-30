af00f97fc35a7a97d8b494d7c31a6ba42f70918f
Thinnakorn Pattha <baleethai@gmail.com>
2019-05-01 01:09:22 +0700

    updated

--- line/login_uselib.php
+++ line/login_uselib.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 
-require_once('LineLoginLib.php');
- 
+ require 'LineLoginLib.php';
 // กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
 // ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
