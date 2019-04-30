4e86630473bea156ac8473dde1a759c9e333e158
Thinnakorn Pattha <baleethai@gmail.com>
2019-05-01 01:07:27 +0700

    updated

--- line/login_uselib.php
+++ line/login_uselib.php
@@ -6,7 +6,7 @@ require_once('LineLoginLib.php');
 // กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
 // ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// error_reporting(E_ALL);
  
 // กรณีมีการเชื่อมต่อกับฐานข้อมูล
 //require_once("dbconnect.php");
